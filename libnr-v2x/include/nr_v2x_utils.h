d6528c95319c41bfb66347a956b49249368003af
wcseo <wcseo.kr@gmail.com>
2026-03-16 15:37:19 +0900

    libnr-v2x 소스코드 내용 최적화 (불필요 코드 삭제)

--- libnr-v2x/include/nr_v2x_utils.h
+++ libnr-v2x/include/nr_v2x_utils.h
@@ -19,9 +19,5 @@
 uint32_t CLI_UTIL_GetCrc32(const uint8_t* pBuf, size_t unSize);
  
 uint64_t convert_nr_status_us(uint64_t value);
-bool get_modem_tick(const std::vector<nr_v2x_ext_status_msg_field_t> &status, uint64_t &tx, uint64_t &rx);
 
-bool get_tx_modem_info(const std::vector<nr_v2x_ext_status_msg_field_t> &status, nr_v2x_ext_status_modem_tx_t &out);
-bool get_rx_modem_info(const std::vector<nr_v2x_ext_status_msg_field_t> &status, nr_v2x_ext_status_modem_rx_t &out);
-   
 #endif
